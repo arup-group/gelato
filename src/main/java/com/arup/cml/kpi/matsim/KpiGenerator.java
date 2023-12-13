92b26e850560b63fe5364d7fa80343e06d31f57b
Michael Fitzmaurice <Michael.Fitzmaurice@arup.com>
2023-12-13 15:04:44 +0000

    Add a shaded jar file to the build

---
+++ src/main/java/com/arup/cml/kpi/matsim/KpiGenerator.java
@@ -0,0 +1,8 @@
+package com.arup.cml.kpi.matsim;
+
+public class KpiGenerator {
+
+    public static void main(String[] args) {
+        System.out.println("Making KPI metrics...");
+    }
+}
---
+++ src/test/java/com/arup/cml/kpi/matsim/TestNothing.java
@@ -0,0 +1,15 @@
+package com.arup.cml.kpi.matsim;
+
+
+import org.junit.Test;
+
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class TestNothing {
+
+    @Test
+    public void testNothing() {
+
+    }
+}
